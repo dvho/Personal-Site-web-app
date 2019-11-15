96ece04e0e43158fb0399cfb91117c31579ddab1
David <allhits@gmail.com>
2019-11-14 20:13:11 -0500

    Update build for GitHub pages

---
+++ precache-manifest.a3dbcbdfa366128309e03a6570ad0876.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c56e44c9023a0e3f020b5f3fa633462",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e10f4b605e3dc294389e",
+    "url": "/static/css/main.461db0a0.chunk.css"
+  },
+  {
+    "revision": "127ba71866478f5b058e",
+    "url": "/static/js/2.1865083a.chunk.js"
+  },
+  {
+    "revision": "e10f4b605e3dc294389e",
+    "url": "/static/js/main.90e89489.chunk.js"
+  },
+  {
+    "revision": "7f7acf4b2d7f5a75426e",
+    "url": "/static/js/runtime-main.a9d3e950.js"
+  },
+  {
+    "revision": "1a95c6352b06bcfd06d9461bab092919",
+    "url": "/static/media/back-trees-only.1a95c635.png"
+  },
+  {
+    "revision": "34db220d3ac704863bc39aa98acf1e23",
+    "url": "/static/media/back.34db220d.png"
+  },
+  {
+    "revision": "f2c27190d68b38a59ef9819d8cd8767b",
+    "url": "/static/media/main.f2c27190.png"
+  },
+  {
+    "revision": "17955c04fde395bd50ee64debb620986",
+    "url": "/static/media/veil.17955c04.png"
+  }
+]);
