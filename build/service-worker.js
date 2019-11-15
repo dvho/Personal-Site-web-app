aee99dce59a7c2e37b27d934eab58bf0e8c6eaac
David <allhits@gmail.com>
2019-11-14 21:10:37 -0500

    Disable mobile zoom because it is overriding overflow hidden

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8df43e0c4fffc15dcdf9ddac13513b02.js"
+  "./precache-manifest.8def7612d26ee4bfa865d4b370963754.js"
 );
 
 self.addEventListener('message', (event) => {
