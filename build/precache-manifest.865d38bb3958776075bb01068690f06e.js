6d8c3dfa5bac180b0527a04f691bb25d7c547d2b
David <allhits@gmail.com>
2019-11-14 20:38:27 -0500

    Add "homepage": ".", to package.json

---
+++ build/precache-manifest.865d38bb3958776075bb01068690f06e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a21ece88cd07ea268c2603ecfedb004",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c80ce47412dc5085c6a",
+    "url": "./static/css/main.461db0a0.chunk.css"
+  },
+  {
+    "revision": "127ba71866478f5b058e",
+    "url": "./static/js/2.1865083a.chunk.js"
+  },
+  {
+    "revision": "9c80ce47412dc5085c6a",
+    "url": "./static/js/main.50edaf30.chunk.js"
+  },
+  {
+    "revision": "e62221520365b315da78",
+    "url": "./static/js/runtime-main.4b4cc7f5.js"
+  },
+  {
+    "revision": "1a95c6352b06bcfd06d9461bab092919",
+    "url": "./static/media/back-trees-only.1a95c635.png"
+  },
+  {
+    "revision": "34db220d3ac704863bc39aa98acf1e23",
+    "url": "./static/media/back.34db220d.png"
+  },
+  {
+    "revision": "f2c27190d68b38a59ef9819d8cd8767b",
+    "url": "./static/media/main.f2c27190.png"
+  },
+  {
+    "revision": "17955c04fde395bd50ee64debb620986",
+    "url": "./static/media/veil.17955c04.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44933c723db6ae6dc7078180bcae4082.js"
+  "./precache-manifest.865d38bb3958776075bb01068690f06e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
