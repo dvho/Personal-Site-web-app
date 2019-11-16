8038a62cf530d2fcf25ffd49de14663df9686ef9
David <allhits@gmail.com>
2019-11-16 07:55:51 -0500

    Add assets-in-progress

---
+++ build/precache-manifest.6f0e40c3fcc0f482ab5d608785461e1d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad72caef394a8283a0bec8a8abb7bf80",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8df194eeeefc6b7878e2",
+    "url": "./static/css/main.9926c5ab.chunk.css"
+  },
+  {
+    "revision": "127ba71866478f5b058e",
+    "url": "./static/js/2.1865083a.chunk.js"
+  },
+  {
+    "revision": "8df194eeeefc6b7878e2",
+    "url": "./static/js/main.d025581f.chunk.js"
+  },
+  {
+    "revision": "e62221520365b315da78",
+    "url": "./static/js/runtime-main.4b4cc7f5.js"
+  },
+  {
+    "revision": "1a95c6352b06bcfd06d9461bab092919",
+    "url": "./static/media/back-trees-only.1a95c635.png"
+  },
+  {
+    "revision": "34db220d3ac704863bc39aa98acf1e23",
+    "url": "./static/media/back.34db220d.png"
+  },
+  {
+    "revision": "f2c27190d68b38a59ef9819d8cd8767b",
+    "url": "./static/media/main.f2c27190.png"
+  },
+  {
+    "revision": "17955c04fde395bd50ee64debb620986",
+    "url": "./static/media/veil.17955c04.png"
+  }
+]);
