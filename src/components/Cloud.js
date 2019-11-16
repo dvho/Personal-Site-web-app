97f513332901b4e7acaf8dd1ba9772476310dbfc
David <allhits@gmail.com>
2019-11-16 00:44:14 -0500

    Get Cloud.js triggering Veil.js

--- src/components/Cloud.js
+++ src/components/Cloud.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-class Cloud extends React.Component {
+class Cloud extends React.PureComponent {
     constructor(props) {
         super(props)
         this.state = {
@@ -10,21 +10,32 @@ class Cloud extends React.Component {
 
     render() {
 
+        let color = Math.floor(Math.random() * 360)
+        let size = this.props.canvasHeight / (2 + Math.random() * 4)
+        let opacity = .1 + Math.random() * .6
+        let zIndex = Math.random() * 9000
+        let travelDuration = 32 + Math.random() * 32
+        let animationNumber = Math.ceil(Math.random() * 32)
+
+        this.props.dimVeil(travelDuration, size)
+
         return(
 
             <div
                 className='clouds'
+                id='cloud'
                 style={{
-                    top: '50%'
+                    top: '50%',
                     }}>
 
                 <i
                     className='fa fa-cloud'
                     style={{
-                        fontSize: '10px',
-                        color: 'rgb(255,255,255)',
-                        // *transform: 'rotateY(180deg) translateY(-50%) translateX(-100vw)', *//
-                        animation: `motionAndSize1 ${this.props.travelDuration}s linear`
+                        fontSize: `${size}px`,
+                        color: `rgba(${color},${color},${color},${opacity})`,
+                        position: 'absolute',
+                        zIndex: `${zIndex}`,
+                        animation: `motionSizeAndFlip${animationNumber} ${travelDuration}s linear forwards`
                         }}>
                 </i>
 
