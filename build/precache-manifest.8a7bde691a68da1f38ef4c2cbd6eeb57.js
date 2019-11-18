25dad12d2137f0579b7152aae9931dd59fa18c39
David <allhits@gmail.com>
2019-11-17 19:04:43 -0500

    Start face test

---
+++ build/precache-manifest.8a7bde691a68da1f38ef4c2cbd6eeb57.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c83b724a0161e2818de69512a7ecf0f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e048f2040bfe019313f",
+    "url": "./static/css/main.4ec5bea5.chunk.css"
+  },
+  {
+    "revision": "998af6c3ac787e310f6f",
+    "url": "./static/js/2.3348af26.chunk.js"
+  },
+  {
+    "revision": "7e048f2040bfe019313f",
+    "url": "./static/js/main.9dc6d628.chunk.js"
+  },
+  {
+    "revision": "e62221520365b315da78",
+    "url": "./static/js/runtime-main.4b4cc7f5.js"
+  },
+  {
+    "revision": "1a95c6352b06bcfd06d9461bab092919",
+    "url": "./static/media/back-trees-only.1a95c635.png"
+  },
+  {
+    "revision": "34db220d3ac704863bc39aa98acf1e23",
+    "url": "./static/media/back.34db220d.png"
+  },
+  {
+    "revision": "26d5f71fb4285399779f17e064f830f4",
+    "url": "./static/media/face-test.26d5f71f.png"
+  },
+  {
+    "revision": "f2c27190d68b38a59ef9819d8cd8767b",
+    "url": "./static/media/main.f2c27190.png"
+  },
+  {
+    "revision": "17955c04fde395bd50ee64debb620986",
+    "url": "./static/media/veil.17955c04.png"
+  }
+]);
---
+++ src/components/Face.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import '../App.css'
+import config from '../config'
+
+//Float point conversion was giving values like '8.9999999999999' hence the verbose localOpacity = `${((Math.round(this.props.opacity * 10))/10).toString()}`
+
+class Face extends React.PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        let localOpacity
+        if (this.props.opacity > .25) {
+            localOpacity = '.25'
+        } else {
+            localOpacity = `${((Math.round(this.props.opacity * 10))/10).toString()}`
+        }
+
+        return(
+
+            <img
+                alt={'face'}
+                src={config.images.faceTest}
+                className='canvas'
+                style={{
+                    opacity: localOpacity,
+                    transition: 'opacity 8s ease'
+                    }}/>
+        )
+    }
+}
+
+export default Face
