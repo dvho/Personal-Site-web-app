d403ff9c59e766aa10c8b25bab88923b6b8bbb5b
David <allhits@gmail.com>
2019-12-08 01:44:36 -0500

    Integrate user engagement

--- src/App.js
+++ src/App.js
@@ -19,16 +19,23 @@ class App extends React.PureComponent {
             canvasHeight: 0,
             canvasWidth: 0,
             moonDiameter: 0,
+            xCoord: -1,
+            yCoord: -1,
             cloudNumber: 0,
             veilOpacity: .3,
             blinkActive: false,
             eyesJustSwitched: false,
-            faceFrame: config.images.faceEmpty
+            faceFrame: config.images.eyePosition.faceEmpty,
+            t: null
         }
     }
 
-    switchEyePosition = () => {
-        let position = Object.entries(config.images)[5 + Math.floor(Math.random() * 37)][1] //Object.entries is a method takes an object and returns an array whose elements are arrays of that objects key value pairs (i.e. each element is a [key, value]). The value is the image itself (it console.logs as a base64) so Object.entries(config.images)[0][1] would return the 1st image, Object.entries(config.images)[2][1] the 3rd, etc. In this case the first key value pairs in the config.images object are not for the face so we start with Object.entries(config.images)[5][1], which is faceEmpty.
+    userSwitchEyePosition = () => {
+        console.log(this.state.xCoord, this.state.yCoord)
+    }
+
+    autoSwitchEyePosition = () => {
+        let position = Object.entries(config.images.eyePosition)[Math.floor(Math.random() * 37)][1] //Object.entries is a method that takes an object and returns an array whose elements are arrays of that object's key value pairs (i.e. each element is a [key, value]). The value is the image itself (it console.logs as a base64) so Object.entries(config.images.eyePosition)[0][1] would return the 1st image, Object.entries(config.images.eyePosition)[2][1] the 3rd, etc.
         let holdEyePosition = 500 + Math.random() * 500
         this.setState({faceFrame: position, eyesJustSwitched: true}) //Switch eyesJustSwitched to true
         setTimeout(() => {this.setState({eyesJustSwitched: false})}, holdEyePosition) //Wait 500 + Math.random() * 500 to switch eyesJustSwitched to false
@@ -41,40 +48,40 @@ class App extends React.PureComponent {
 
         //First 2 setTimeouts below should be commented out. A blink looks more natural when eyes move rapidly to close and drag more on the way up, and omitting those frames depicted that much better.
         this.setState({blinkActive: true}) //Make blinkActive true
-        this.setState({faceFrame: config.images.faceEmpty}) //Set faceFrame to faceEmpty, which reveals eyes facing forward, until first setTimeout is called in blinkDuration * .09
+        this.setState({faceFrame: config.images.eyePosition.faceEmpty}) //Set faceFrame to faceEmpty, which reveals eyes facing forward, until first setTimeout is called in blinkDuration * .09
 
         // setTimeout(() => {
-        //     this.setState({faceFrame: config.images.faceBlinkF})
+        //     this.setState({faceFrame: config.images.blink.faceBlinkF})
         // }, blinkDuration * .02)
         // setTimeout(() => {
-        //     this.setState({faceFrame: config.images.faceBlinkE})
+        //     this.setState({faceFrame: config.images.blink.faceBlinkE})
         // }, blinkDuration * .05)
         setTimeout(() => {
-            this.setState({faceFrame: config.images.faceBlinkD})
+            this.setState({faceFrame: config.images.blink.faceBlinkD})
         }, (blinkStareTimeCoefficient + blinkDuration * .09))
         setTimeout(() => {
-            this.setState({faceFrame: config.images.faceBlinkC})
+            this.setState({faceFrame: config.images.blink.faceBlinkC})
         }, (blinkStareTimeCoefficient + blinkDuration * .14))
         setTimeout(() => {
-            this.setState({faceFrame: config.images.faceBlinkB})
+            this.setState({faceFrame: config.images.blink.faceBlinkB})
         }, (blinkStareTimeCoefficient + blinkDuration * .20))
         setTimeout(() => {
-            this.setState({faceFrame: config.images.faceBlinkA})
+            this.setState({faceFrame: config.images.blink.faceBlinkA})
         }, (blinkStareTimeCoefficient + blinkDuration * .27))
         setTimeout(() => {
-            this.setState({faceFrame: config.images.faceBlinkB})
+            this.setState({faceFrame: config.images.blink.faceBlinkB})
         }, (blinkStareTimeCoefficient + blinkDuration * .36))
         setTimeout(() => {
-            this.setState({faceFrame: config.images.faceBlinkC})
+            this.setState({faceFrame: config.images.blink.faceBlinkC})
         }, (blinkStareTimeCoefficient + blinkDuration * .47))
         setTimeout(() => {
-            this.setState({faceFrame: config.images.faceBlinkD})
+            this.setState({faceFrame: config.images.blink.faceBlinkD})
         }, (blinkStareTimeCoefficient + blinkDuration * .60))
         setTimeout(() => {
-            this.setState({faceFrame: config.images.faceBlinkE})
+            this.setState({faceFrame: config.images.blink.faceBlinkE})
         }, (blinkStareTimeCoefficient + blinkDuration * .75))
         setTimeout(() => {
-            this.setState({faceFrame: config.images.faceBlinkF})
+            this.setState({faceFrame: config.images.blink.faceBlinkF})
         }, (blinkStareTimeCoefficient + blinkDuration * .92))
         setTimeout(() => {
             this.setState({faceFrame: current, blinkActive: false}) //Make blinkActive false again
@@ -103,23 +110,60 @@ class App extends React.PureComponent {
         }, cloudOut)
     }
 
-    calcAllDimensionsAndResetClouds = () => { //Get the size of the screen, canvas, and moon on both load and on resize
+    timer = () => {
+        this.setState({ //this.timer is called whenever the mouseover, mousemove, touchstart, or touchmove events fire, which sets this.state.t to the function that, in turn, sets this.state.xCoords and this.state.yCoords to -1. Subsequently, every time this.timer is called, clearTimeout is called on this.state.t, which is accessible from this.calcAllDimensionsCoordsAndResetClouds because it is simply a key in state.
+            t: setTimeout(() => {
+                this.setState({
+                    xCoord: -1,
+                    yCoord: -1
+                })
+            }, 8000)
+        })
+    }
+
+    calcAllDimensionsCoordsAndResetClouds = (e) => {
+
         let screenWidth = window.innerWidth
         let canvasHeight = window.innerHeight
-        let canvasWidth = Math.round(canvasHeight * 1.323572474377745) // screenWidth < canvasHeight * 1.323572474377745 ? screenWidth : Math.round(canvasHeight * 1.323572474377745)
+        let canvasWidth = Math.round(canvasHeight * 1.323572474377745) //screenWidth < canvasHeight * 1.323572474377745 ? screenWidth : Math.round(canvasHeight * 1.323572474377745)
         let moonDiameter = Math.round(canvasWidth * 0.199121522693997)
-        this.setState({
-            screenWidth: screenWidth,
-            canvasHeight: canvasHeight,
-            canvasWidth: canvasWidth,
-            moonDiameter: moonDiameter,
-            cloudNumber: 0
-        })
+
+        if (e.type === 'load' || e.type === 'resize') { //Get the sizes of the screen, canvas, and moon, and reset cloudNumber on both load and on resize
+            this.setState({
+                screenWidth: screenWidth,
+                canvasHeight: canvasHeight,
+                canvasWidth: canvasWidth,
+                moonDiameter: moonDiameter,
+                cloudNumber: 0
+            })
+        } else if (e.type === 'mouseover' || e.type === 'mousemove' || e.type === 'touchstart' || e.type === 'touchmove') { //Get the X and Y positions on mouseover, mousemove, touchstart, and touchmove
+            let margin = ((screenWidth - canvasWidth) / 2)
+            let yCoord = e.clientY / canvasHeight
+            let xCoord
+
+            if (margin > 0) { //If there's a margin, calculate the xCoord of just the event over the canvasWidth
+                xCoord = (e.clientX - margin) / canvasWidth
+            } else {
+                xCoord = e.clientX / screenWidth //else if there's no margin the visible canvasWidth is the same as the screenWidth
+            }
+
+            if (xCoord >= 0 && xCoord <= 1 && (this.state.xCoord !== xCoord || this.state.yCoord !== yCoord)) { //If the xCoord is between 0 and 1 and either the xCoord or yCoord has changed
+
+                this.setState({ //set the new xCoord and yCoord in state
+                    xCoord: xCoord,
+                    yCoord: yCoord
+                })
+
+                clearTimeout(this.state.t) //clearTimeout of this.state.t
+                this.timer() //restart the this.state.t timeOut function by calling its parent function, this.timer
+                this.userSwitchEyePosition() //call this.userSwitchEyePosition and know that autoSwitchEyePosition isn't being called from either autoSwitchEyePositionControl nor blinkControl because this.state.xCoords would have to === -1 (and, by extention, this.state.yCoords would have to === -1) in order for that to happen.
+            }
+        }
     }
 
     componentDidMount() {
         //Fire up event listeners when App.js mounts
-        ['load', 'resize'].forEach(i => window.addEventListener(i, this.calcAllDimensionsAndResetClouds))
+        ['load', 'resize', 'mouseover', 'mousemove', 'touchstart', 'touchmove'].forEach(i => window.addEventListener(i, this.calcAllDimensionsCoordsAndResetClouds))
         //Fire up the cloud's "game loop" as a controller that calls itself randomly between 1 and 9s and, in the interim, increases the cloudNumber by 1
         let cloudControl
 
@@ -134,20 +178,22 @@ class App extends React.PureComponent {
         let blinkControl
         (blinkControl = () => {
             let repeatRate = 875 + Math.random() * 3000 //Repeat rate of blinkControl must be less than the max time it would take to blink, which is blinkStareTimeCoefficient + blinkDuration * 1.1 in the blink method, which is called below
-            if (!this.state.eyesJustSwitched) { //If eyesJustSwitched is false (i.e. it's been longer than holdEyePosition, which is between 500ms and 1000ms, since eye position has switched) then switch eye position again and blink.
-                this.switchEyePosition()
+            if (!this.state.eyesJustSwitched) { //If eyesJustSwitched is false (i.e. it's been longer than holdEyePosition, which is between 500ms and 1000ms, since eye position has switched) then blink, and if this.state.xCoord === -1, which means it's been longer than 8000ms since mouseover, mousemove, touchstart, or touchmove events have fired, switch eye position again.
+                if (this.state.xCoord === -1) {
+                    this.autoSwitchEyePosition()
+                }
                 this.blink()
             }
             setTimeout(blinkControl, repeatRate)
         })()
 
-        let switchEyePositionControl
-        (switchEyePositionControl = () => {
+        let autoSwitchEyePositionControl
+        (autoSwitchEyePositionControl = () => {
             let repeatRate = 1000 + Math.random() * 2000
-            if (!this.state.blinkActive && !this.state.eyesJustSwitched) { //If blink is not active and eyes were not just switched (i.e. it's been longer than holdEyePosition, which is between 500ms and 1000ms) only then can you run switchEyePosition. Note, switchEyePosition can run from blinkControl as well so the condition !this.state.eyesJustSwitched must be reiterated here even through repeatRate for switchEyePositionControl is greater than 1000ms.
-                this.switchEyePosition()
+            if (!this.state.blinkActive && !this.state.eyesJustSwitched && this.state.xCoord === -1) { //If blink is not active and eyes were not just switched (i.e. it's been longer than holdEyePosition, which is between 500ms and 1000ms) and this.state.xCoord === -1, which means it's been longer than 8000ms since mouseover, mousemove, touchstart, or touchmove events have fired only then can you run autoSwitchEyePosition. Note, autoSwitchEyePosition can run from blinkControl as well so the condition !this.state.eyesJustSwitched must be reiterated here even through repeatRate for autoSwitchEyePositionControl is greater than 1000ms.
+                this.autoSwitchEyePosition()
             }
-            setTimeout(switchEyePositionControl, repeatRate)
+            setTimeout(autoSwitchEyePositionControl, repeatRate)
         })()
     }
 
@@ -163,13 +209,13 @@ class App extends React.PureComponent {
         return (
             <div className="canvas-parent">
 
-                <img alt={"back"} src={config.images.back} className="canvas" id="back-image"/>
+                <img alt={"back"} src={config.images.canvas.back} className="canvas" id="back-image"/>
 
                 {allClouds}
 
-                <img alt={"back trees only"} src={config.images.backTreesOnly} className="canvas" id="back-trees-only-image"/>
+                <img alt={"back trees only"} src={config.images.canvas.backTreesOnly} className="canvas" id="back-trees-only-image"/>
 
-                <img alt={"main"} src={config.images.main} className="canvas" id="main-image"/>
+                <img alt={"main"} src={config.images.canvas.main} className="canvas" id="main-image"/>
 
                 <Veil opacity={this.state.veilOpacity} key={'a'}/>
 
