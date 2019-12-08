6dd8c0aafd25361d47273ee278a8ad2c981401c5
David <allhits@gmail.com>
2019-12-08 01:44:36 -0500

    Integrate user engagement

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5d416d601a0db039440f134ba69aed39.js"
+  "./precache-manifest.f1d5db4b5cfe8dc607b3dda2c78b6e7f.js"
 );
 
 self.addEventListener('message', (event) => {
