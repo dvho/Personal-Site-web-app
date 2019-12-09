92a6216dcb63dae20a73942a4a6ee9dad2426da2
David <allhits@gmail.com>
2019-12-09 12:55:37 -0500

    Test touch events

--- build/precache-manifest.e6570b21bab8f66d4e8bb64d68e653e4.js
+++ build/precache-manifest.e6570b21bab8f66d4e8bb64d68e653e4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "43e8618b38966b5c89a4de861adc3fa7",
+    "revision": "4748ca123f781069500a661f0a9de540",
     "url": "./index.html"
   },
   {
-    "revision": "aa856485bc153da3bd20",
+    "revision": "571120859a9d1c7f9966",
     "url": "./static/css/main.6dc5bf42.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.3348af26.chunk.js"
   },
   {
-    "revision": "aa856485bc153da3bd20",
-    "url": "./static/js/main.cf396684.chunk.js"
+    "revision": "571120859a9d1c7f9966",
+    "url": "./static/js/main.50671b1b.chunk.js"
   },
   {
     "revision": "e62221520365b315da78",
