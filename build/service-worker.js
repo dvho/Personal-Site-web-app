065f6bbf2dbd62ea0e1bd39f8246300c8426351e
David <allhits@gmail.com>
2019-12-09 13:51:16 -0500

    Test touch events

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e5284b57392c67eff37aa90357dd6717.js"
+  "./precache-manifest.1b3b0a7430ce94d7495293253253c37f.js"
 );
 
 self.addEventListener('message', (event) => {
