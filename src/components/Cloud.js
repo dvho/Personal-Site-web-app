de2e771fe0b7bf57aae97be57fc8d5a21480c73e
David <allhits@gmail.com>
2019-12-09 19:18:30 -0500

    Build out userSwitchEyePosition method

--- src/components/Cloud.js
+++ src/components/Cloud.js
@@ -38,7 +38,7 @@ class Cloud extends React.PureComponent {
                     style={{
                         fontSize: `${size}px`,
                         color: `hsla(${color}, 100%, 96%, ${opacity})`,
-                        filter: this.props.performanceBoost ? null : `drop-shadow(0px 0px ${cloudHaze}px hsla(${color}, 100%, 96%, 1)`,   /* Comment out filter property if you need to boost performance during developent. CPU and GPU really strain to animate this drop shadow. */
+                        filter: this.props.performanceBoost ? null : `drop-shadow(0px 0px ${cloudHaze}px hsla(${color}, 100%, 96%, 1)`,
                         position: 'absolute',
                         zIndex: `${zIndex}`,
                         animation: `motionSizeAndFlip${animationNumber} ${travelDuration}s linear forwards`
