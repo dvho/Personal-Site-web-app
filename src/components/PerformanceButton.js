c78752b02729b00006e936ffe2b23f9131291ec5
David <allhits@gmail.com>
2019-12-12 20:10:57 -0500

    Add ripple, click handler, and extract performance button to common component

---
+++ src/components/PerformanceButton.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../App.css'
+import config from '../config'
+
+class PerformanceButton extends React.PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+
+        let containerDiameter = this.props.state.performanceButtonDiameter * 2
+        let buttonDiameter = this.props.state.performanceButtonDiameter
+        let backgroundColor = this.props.state.performanceBoost ? 'rgb(128,128,0)' : 'rgb(255,255,255)'
+
+        return(
+            <div className="performanceButtonContainer" style={{width: containerDiameter, height: containerDiameter}} onClick={() => this.props.togglePerformanceBoost()}>
+                <div className="performanceButton" style={{backgroundColor: backgroundColor, width: buttonDiameter, height: buttonDiameter, margin: this.props.state.performanceButtonDiameter /2}}></div>
+            </div>
+        )
+    }
+}
+
+export default PerformanceButton
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Cloud from './Cloud'
 import Veil from './Veil'
 import Face from './Face'
+import PerformanceButton from './PerformanceButton'
 
 export {
     Cloud,
     Veil,
-    Face
+    Face,
+    PerformanceButton
 }
