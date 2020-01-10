8213fba4e7e00a7e966ff81b791cdd54720db0c6
David <allhits@gmail.com>
2020-01-10 12:37:06 -0500

    Add InfoSheet.js

--- src/components/InfoSheet.js
+++ src/components/InfoSheet.js
@@ -10,15 +10,14 @@ class InfoSheet extends React.PureComponent {
 
     render() {
 
-        let left = this.props.state.canvasWidth < this.props.state.screenWidth ? this.props.state.margin + this.props.state.canvasWidth - 243 : this.props.state.screenWidth - 243
-        let height = this.props.state.canvasHeight * .8
-        let className = this.props.state.revealInfoSheet ? 'info-sheet-container info-sheet-container-revealed' : 'info-sheet-container'
-
+        let left = this.props.canvasWidth < this.props.screenWidth ? this.props.margin + this.props.canvasWidth - 270 : this.props.screenWidth - 270
+        let height = this.props.canvasHeight * .8
+        let className = this.props.revealInfoSheet ? 'info-sheet-container info-sheet-container-revealed' : 'info-sheet-container'
 
         return(
             <div className={className} style={{left: left, height: height}}>
-                <h className='info-sheet-heading'>How did I build this?</h>
-                <p>I built this app with React (and ReactPlayer, for the audio component). There are no other dependencies! Play any of my songs (still works in progress) from the interface below or click their titles.</p>
+                <h1 className='info-sheet-heading'>How did I build this?</h1>
+                <p>I built this app with React (and ReactPlayer for the audio component). There are no other dependencies! Play any of my songs (still works in progress) from the interface below or click their titles.</p>
                 <p>When clouds outside pass in front of the moon they dim the moonlight entering the room and reveal my animated face, which you can interact with by mouse or touch. The visibility of my face is commensurate with the darkness of the room at any given moment. </p>
                 <p>Click the contact menu on the left to send me an email, view my resume, or check out some of my other projects on GitHub and NPM. You can toggle the performance button at the top right to add haziness to the clouds, the default for which is off since this does strain your GPU.</p>
             </div>
