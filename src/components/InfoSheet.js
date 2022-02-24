9dac9622c3265167056d5546dae988cfff38ff2e
dvho <email6@gmail.com>
2022-02-24 17:52:53 -0500

    Update my recordings, refactor folders, integrate devTest prop, and update robots.txt

--- src/components/InfoSheet.js
+++ src/components/InfoSheet.js
@@ -13,8 +13,8 @@ const InfoSheet = props => {
         return(
             <div className={className} style={{left: left, height: height}}>
                 <h1 className='info-sheet-heading'>How did I build this?</h1>
-                <p>I built this app with React (and ReactPlayer for the audio component). There are no other dependencies! Play any of my songs (still works in progress) from the interface below or click their titles.</p>
-                <p>When clouds outside pass in front of the moon they dim the moonlight entering the room and reveal my animated face, which you can interact with by mouse or touch. The visibility of my face is commensurate with the darkness of the room at any given moment. </p>
+                <p>I built this with React! Play any of my songs (still works in progress) from the interface below or click their titles.</p>
+                <p>When clouds outside pass in front of the moon they dim the moonlight entering the room and reveal my animated face, which you can interact with by mouse or touch. The visibility of my face is commensurate with the darkness of the room at any given moment.</p>
                 <p>Click the contact menu on the left to send me an email, view my resume, or check out some of my other projects on GitHub and NPM. You can toggle the performance button at the top right to add haziness to the clouds, the default for which is off since this does strain your GPU.</p>
             </div>
         )
