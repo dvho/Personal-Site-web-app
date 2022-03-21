ca1f2c28abc3dc3fa11bfdb837aa3793d9cd8b28
dvho <email6@gmail.com>
2022-03-21 19:53:01 -0400

    Update App.js to use HashRouter from react-router-dom instead of BrowserRouter, and update lyrics

---
+++ precache-manifest.fe072e3b4786b9c4cf313a1ff680df6f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aca8b7365ffe3c6edf8f499cc9ef08e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d380eca04f17203b02a6",
+    "url": "/static/css/main.85870e61.chunk.css"
+  },
+  {
+    "revision": "98f9eb752cce92f80496",
+    "url": "/static/js/2.692dff34.chunk.js"
+  },
+  {
+    "revision": "d380eca04f17203b02a6",
+    "url": "/static/js/main.12628f4c.chunk.js"
+  },
+  {
+    "revision": "65b05c915e08fa05e73e",
+    "url": "/static/js/runtime-main.955ae6fc.js"
+  },
+  {
+    "revision": "fcf0bc20b830e709ad3b0828400c2cf2",
+    "url": "/static/media/Baby Elephant-07.fcf0bc20.mp3"
+  },
+  {
+    "revision": "f3cb2182844b95a8ba91a53edd5aa3bb",
+    "url": "/static/media/Bad Place-08.f3cb2182.mp3"
+  },
+  {
+    "revision": "b3a3cd6eeddfc0a7c6417d13454fd7de",
+    "url": "/static/media/Believe in You-35.b3a3cd6e.mp3"
+  },
+  {
+    "revision": "f6f358c5ce7cd9e37ea3200960f4e8a0",
+    "url": "/static/media/Driving and Crying-16.f6f358c5.mp3"
+  },
+  {
+    "revision": "36c120c8b557bc374c682f96d49ddee3",
+    "url": "/static/media/Fire and Acid Rain-24.36c120c8.mp3"
+  },
+  {
+    "revision": "261b80dba7de07af8b06d20d2314593e",
+    "url": "/static/media/Highest Mountain-18.261b80db.mp3"
+  },
+  {
+    "revision": "f3afd10bd884a95961f44e3b5837d1fb",
+    "url": "/static/media/How Could You Say That-38.f3afd10b.mp3"
+  },
+  {
+    "revision": "6e3d7bb491b5f6f0dfc74c43c006443e",
+    "url": "/static/media/I Will Not Forgive-18.6e3d7bb4.mp3"
+  },
+  {
+    "revision": "6a24da45f3d4c5543f1afd65b5e03782",
+    "url": "/static/media/I Wish It Were Raining-23.6a24da45.mp3"
+  },
+  {
+    "revision": "1a95c6352b06bcfd06d9461bab092919",
+    "url": "/static/media/back-trees-only.1a95c635.png"
+  },
+  {
+    "revision": "34db220d3ac704863bc39aa98acf1e23",
+    "url": "/static/media/back.34db220d.png"
+  },
+  {
+    "revision": "a4a34a3c8ae35f6e84bb878e2ab2c9a0",
+    "url": "/static/media/face-main.a4a34a3c.png"
+  },
+  {
+    "revision": "f2c27190d68b38a59ef9819d8cd8767b",
+    "url": "/static/media/main.f2c27190.png"
+  },
+  {
+    "revision": "17955c04fde395bd50ee64debb620986",
+    "url": "/static/media/veil.17955c04.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ceaff839a7048abe99bbafce4b283586.js"
+  "/precache-manifest.fe072e3b4786b9c4cf313a1ff680df6f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
