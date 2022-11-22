c583254ce9c151cfb18f90a6aaaaebfd46e90cb4
dvho <email6@gmail.com>
2022-11-22 02:00:44 -0500

    Install react-photo-view, create date.js, create TravelPhotos.js, create utils/index.js, add travel icon to SocialMenu.js, adjust setTimeout rate in onHold of Track.js, build out App.css for TravelPhotos.js, and refactor App.js

--- src/App.js
+++ src/App.js
@@ -2,27 +2,36 @@ import { HashRouter, Route, Routes } from 'react-router-dom'
 
 
 import Home from './Home'
-import { SingleTrack } from './components'
+import { SingleTrack, TravelPhotos } from './components'
 
 import config from './config'
 
 const App = () => {
 
-    let hashes, index, path, track
+    let hashes, index, path, track, element
 
-    hashes = config.tracks.map(i => `#/${i.slug}`)
-    index = hashes.indexOf(window.location.hash)
+    hashes = config.tracks.map(i => `#/${i.slug}`) //Make hashes from each of the slugs in    config.tracks[n].slug
+    hashes.push('#/journeys') //...and push    #/journeys    as the final one.
 
-    if (config.tracks.filter(i => `#/${i.slug}` === window.location.hash).length > 0) {
+    index = hashes.indexOf(window.location.hash) //...Because the hashes array begins with making hashes from the tracks in numerical order before    #/journeys    is pushed to the hashes array your    indexOf(window.location.hash)    will be the index...
+
+    if (config.tracks.filter(i => `#/${i.slug}` === window.location.hash).length === 1) {
         path = window.location.hash.replace('#', '')
-        track = config.tracks[index]
+        track = config.tracks[index] //...that you can pass here in order to get the track...
+        element = (() => <SingleTrack track={track}/>)() //...to pass here.
+    } else if (window.location.hash === '#/journeys') {
+        path = window.location.hash.replace('#', '')
+        element = (() => <TravelPhotos/>)()
+    } else {
+        path = window.location.hash.replace('#', '')
+        element = (() => <Home/>)()
     }
 
     return (
         <HashRouter>
             <Routes>
-                <Route path='*' element={<Home />} />
-                <Route path={path} element={<SingleTrack track={track}/>}/>
+                { /* <Route path='*' element={<Home />} /> */ }
+                <Route path={path} element={element}/>
             </Routes>
         </HashRouter>
     )
